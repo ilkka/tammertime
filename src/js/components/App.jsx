780a189d8d6f070443dea64b155427db58d6284f
Ilkka Laukkanen <ilkka@ilkka.io>
2015-06-16 15:38:06 +0300

    More of the fixing

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 const BGURL = require('../../img/tampere.png');
 
+const BGW = 200;
+const BGH = 227;
+
+function randomizePos(e) {
+  let x = Math.floor(Math.random() * (window.innerWidth - BGW + 1));
+  let y = Math.floor(Math.random() * (window.innerHeight - BGH + 1));
+  if (x < e.clientX && (x + BGW > e.clientX) &&
+      y > e.clientY && (y + BGH > e.clientY)
+  ) {
+    return randomizePos(e);
+  } else {
+    return { x, y };
+  }
+}
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      width: BGW,
+      height: BGH,
+
       x: Math.random() * window.innerWidth,
       y: Math.random() * window.innerHeight
     }
@@ -13,20 +31,19 @@ export default class App extends React.Component {
 
   render() {
     let style = {
+      width: BGW,
+      height: BGH,
       left: this.state.x,
       top: this.state.y,
       backgroundImage: `url(${BGURL})`
     };
 
     return (
-      <span className='touchthis' style={style} onMouseEnter={(e) => this.move(e)} onMouseDown={this.move}>U can't touch this</span>
+      <span className='touchthis' style={style} onMouseMove={(e) => this.move(e)} onMouseDown={this.move}>U can't touch this</span>
     );
   }
 
   move(e) {
-    this.setState({
-      x: Math.random() * window.innerWidth,
-      y: Math.random() * window.innerHeight
-    });
+    this.setState(randomizePos(e));
   }
 }
